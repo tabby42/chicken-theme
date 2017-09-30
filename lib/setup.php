144962905126574ed9c7ad55676b15e6a1ea7789
tabby42 <irene.theiss@gmx.at>
2017-09-30 22:11:21 +0200

    added bootstrap menu to theme

--- lib/setup.php
+++ lib/setup.php
@@ -104,3 +104,6 @@ function assets() {
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
+
+// Register custom navigation walker
+require_once('wp_bootstrap_navwalker.php');
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,39 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+	<a class="navbar-brand js-scroll-trigger" href="<?= esc_url(home_url('/')); ?>"><?//php bloginfo('name'); ?>LOGO</a>
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <!--<ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>-->
+			<?php /* Primary navigation */
+			if (has_nav_menu('primary_navigation')) :
+				wp_nav_menu( array(
+				  'theme_location' => 'primary_navigation',
+				  'menu' => 'top_menu',
+				  'depth' => 2,
+				  'container' => false,
+				  'menu_class' => 'nav',
+				  //Process nav menu using our custom nav walker
+				  'walker' => new wp_bootstrap_navwalker())
+				);
+			endif;
+			?>
+        </div>
+      </div>
     </nav>
-  </div>
 </header>

d5d519a976d82d5632b14e2df9d70a59fb0cce4e
tabby42 <irene.theiss@gmx.at>
2017-11-07 19:50:24 +0100

    added custom.js file and _mixins.scss file

---
+++ assets/scripts/custom.js
@@ -0,0 +1,26 @@
+var $j = jQuery.noConflict();
+
+$j(document).ready(function( $ ) {
+	"use strict";
+    
+    $(window).scroll(function() {
+      //console.log($(window).scrollTop());
+      if ($(window).scrollTop() > 300) {
+        $('nav.navbar').addClass('stickynav');
+      } else {
+        $('nav.navbar').removeClass('stickynav');
+      }
+    });
+    
+    $('.menu-item > a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 750);
+                return false;
+            }
+        }
+    });
+});

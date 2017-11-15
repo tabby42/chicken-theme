d4ece7cd433cd5fd173337e8a85c2a9a2b2a0555
tabby42 <irene.theiss@gmx.at>
2017-11-15 16:39:46 +0100

    added markup for mobile slider

--- templates/homeslider.php
+++ templates/homeslider.php
@@ -4,7 +4,18 @@
             <div class="hidden-sm-down">
                  <?php echo do_shortcode(' [crellyslider alias="homeslider"]');?>
             </div>
-          
+            <div class="hidden-sm-up mobile-slider">
+                <ul class="slides">
+                    <li class="slide">Psychotherapie</li>
+                    <li class="slide">Gestalttherapie</li>
+                    <li class="slide">Gruppentherapie</li>
+                    <li class="slide">Zentherapie</li>
+                    <li class="slide">Multimediale<br>Kunsttherapie</li>
+                    <li class="slide">Traumatherapie</li>
+                    <li class="slide">Supervision, Coaching<br> und Beratung</li>
+                    <li class="slide">Alchemie der Farben</li>
+                </ul>
+            </div>
         </div>
       </div>
 </section>
